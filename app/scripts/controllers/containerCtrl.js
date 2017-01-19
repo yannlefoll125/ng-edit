04303baac28f91dece89b0f904d0e8f4ad06147e
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-01-19 18:02:19 +0100

    made fullscreen first version

--- app/scripts/controllers/containerCtrl.js
+++ app/scripts/controllers/containerCtrl.js
@@ -1,4 +1,8 @@
 app.controller('containerCtrl', ['$scope', function($scope){
+	$scope.fullscreen = false;
 
+	$scope.onFullscreenClick = function() {
+		$scope.fullscreen = !$scope.fullscreen;
+	}
 	
 }]);
